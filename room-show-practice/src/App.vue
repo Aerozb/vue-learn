d89d8f1d5a0adde4d478758a42f37594737084cd
yhy <847888663@qq.com>
2023-09-14 16:44:39 +0800

    房源练习第一次提交

---
+++ room-show-practice/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AreaHeader
+    :title="highScore.title"
+    :subtitle="highScore.subtitle"
+  ></AreaHeader>
+  <<template v-for="item in highScore.list" :key="item.id" >
+    <div>
+      
+    </div>
+  </template>
+</template>
+
+<script setup>
+import highScore from "./data/high_score.json";
+import AreaHeader from "./components/AreaHeader.vue";
+console.log(highScore);
+</script>
+
+<style lang="less" scoped>
+.app {
+  width: 1032px;
+  padding: 40px;
+  margin: 0 auto;
+}
+</style>
---
+++ room-show-practice/src/components/AreaHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <div class="area-header">
+      <div class="title">{{ title }}</div>
+      <div class="subTitle">{{ subtitle }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup> 
+import { defineProps } from 'vue';
+defineProps({
+    title:{
+        type:String,
+        default:"默认标题"
+    },
+    subtitle:{
+        type:String,
+        default:"默认子标题"
+    }
+})
+</script>
+
+<style lang="less" scoped>
+  .area-header {
+    height: 84px;
+                           
+    .title {
+      font-size: 22px;
+    }
+
+    .subtitle {
+      margin-top: 12px;
+      font-size: 16px;
+    }
+  }
+</style>
